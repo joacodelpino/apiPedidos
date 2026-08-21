74fed890e4b08086fc3b356490af52507ff19e07
Joaco <joacodelpino4@gmail.com>
2026-08-21 15:24:33 -0300

    creacion de schema para validacion ded login y signup

--- src/schemas/producto.ts
+++ src/schemas/producto.ts
@@ -67,4 +67,14 @@ export const actualizarEstadoPedidoSchema = z.object({
   estado: z.enum(["pendiente", "confirmado", "entregado", "cancelado"]),
 });
 
-// Validacion para
+// Validacion para email y contraseña
+export const signUpSchema = z.object({
+  email: z.email(),
+  password: z.string().min(8).max(50),
+});
+
+// Validacion para login
+export const loginSchema = z.object({
+  email: z.email(),
+  password: z.string().min(1, "La contraseña es requerida"),
+});
